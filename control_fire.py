342cdc5cb02c85270027281099e4f70a7d93c603
graemet <graeme.john.thomson@gmail.com>
2015-04-29 08:51:29 +0000

    Inital file creation

---
+++ control_fire.py
@@ -0,0 +1,62 @@
+# modules to read from the flirc
+from evdev import InputDevice, categorize, ecodes
+import time
+
+
+# Next: Define the Red, Gren, Yellow and Blue keys on Flirc so we can set temperatures.
+
+ON = True
+OFF = False
+
+# Key definitions
+STOP = 45
+
+class Fire:
+
+    debug = 0
+    required_temperature = 0
+    actual_temperature = 0
+
+    def __init__ (self):
+        self.description = "A fire to heat the home"
+        self.author = "Graeme Thomson"
+
+    def switch_debug_on (self):
+        self.debug = 1
+
+    def switch_debug_off (self):
+        self.debug = 0
+
+    def print_debug_state(self):
+        if self.debug == 1:
+            print 'Debug is ON'
+        else:
+            print 'Debug is OFF'
+
+my_fire = Fire ()
+ 
+my_fire.switch_debug_on ()
+
+my_fire.print_debug_state ()
+
+dev = InputDevice ('/dev/input/event0')
+
+if my_fire.debug == 1:
+  print dev
+# Infinite loop waiting for input from the flirc
+for event in dev.read_loop():
+    #
+    # type should always be 1 for a keypress
+    # code is the numeric value of the key that has been pressed
+    # value 0 = key up, 1 = key down, 2 = key hold
+
+    ticks = time.time()
+    print "Number of ticks since 12:00am, January 1, 1970:", ticks
+
+
+    if event.type == ecodes.EV_KEY:
+        if my_fire.debug == 1:
+            print (categorize(event))
+            print 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value)
+        if event.code == STOP: # stop key on remote
+            break
