04cfe4472222b4c13612a5416acd87ffc7804ee9
graemet <graeme.john.thomson@gmail.com>
2015-04-30 23:31:11 +0000

    Put the remote code in a thread.

--- control_fire.py
+++ control_fire.py
@@ -1,5 +1,7 @@
 # modules to read from the flirc
 from evdev import InputDevice, categorize, ecodes
+from threading import Thread, Event
+import sys
 import time
 
 
@@ -35,20 +37,12 @@ class Fire:
         else:
             print ('Debug is OFF')
 
-my_fire = Fire ()
- 
-my_fire.switch_debug_on ()
 
-my_fire.print_debug_state ()
-
-dev = InputDevice ('/dev/input/event0')
-
-if my_fire.debug == 1:
-  print (dev)
-# Infinite loop waiting for input from the flirc
-while True:
-    ticks = time.time()
-    print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
+def read_remote (debug_on, started_evt ):
+    started_evt.set()
+    dev = InputDevice ('/dev/input/event0')
+    if debug_on == 1:
+        print (dev)
     for event in dev.read_loop():
     	#
         # type should always be 1 for a keypress
@@ -56,10 +50,35 @@ while True:
         # value 0 = key up, 1 = key down, 2 = key hold
 
         if event.type == ecodes.EV_KEY:
-            if my_fire.debug == 1:
+            if debug_on == 1:
                 print (categorize(event))
                 print ( 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value))
             if event.value == 0:  # key up
-                break
-    if event.code == STOP: # stop key on remote
-        break
+                time.sleep(1) 
+        if event.code == STOP: # stop key on remote
+            sys.exit
+
+my_fire = Fire ()
+ 
+my_fire.switch_debug_on ()
+
+my_fire.print_debug_state ()
+
+# Create the event object that will be used to signal startup
+started_evt = Event()
+
+# Launch the thread and pass the startup event
+print('Launching read_remote')
+t = Thread(target=read_remote, args=(my_fire.debug,started_evt))
+t.start()
+
+# Wait for the thread to start
+started_evt.wait()
+print('countdown is running')
+# Infinite loop waiting for input from the flirc
+while True:
+    ticks = time.time()
+    print ("Number of ticks since 12:00am, January 1, 1970:", ticks)
+    time.sleep(2)
+    
+
