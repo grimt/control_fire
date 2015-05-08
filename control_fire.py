40bb434cb77a42e3c9500ee618a63f7506b2c59b
graemet <graeme.john.thomson@gmail.com>
2015-05-08 09:03:16 +0000

    Add debug levels rather than just one level
    Level 5 is the most verbose. Level 1 is the least. Levl 0 is off.

--- control_fire.py
+++ control_fire.py
@@ -5,10 +5,6 @@ import sys
 import time
 
 
-# Next: Move the code that reads the keys to a separate file. Put the desired temperature in this file. Maybe 0 = fire off.
-#     : Leave this file for reading the temperature and maybe responding to web access - or mayb this will also be in a separate file.
-#     : Define the Red, Gren, Yellow and Blue keys on Flirc so we can set temperatures.
-
 ON = True
 OFF = False
 
@@ -22,7 +18,7 @@ REMOTE_KEY_BLUE = 5
 
 class Fire:
 
-    debug = 0
+    debug_level = 0
     required_temperature = 0
     actual_temperature = 0
 
@@ -30,15 +26,12 @@ class Fire:
         self.description = "A fire to heat the home"
         self.author = "Graeme Thomson"
 
-    def switch_debug_on (self):
-        self.debug = 1
-
-    def switch_debug_off (self):
-        self.debug = 0
+    def debug_level_set (self, level):
+        self.debug_level = level
 
     def print_debug_state(self):
-        if self.debug == 1:
-            print ('Debug is ON')
+        if self.debug_level >= 1:
+            print ('Debug is ON level:' + str (self.debug_level))
         else:
             print ('Debug is OFF')
             
@@ -47,6 +40,9 @@ class Fire:
 		
     def desired_temp_get (self):
 	return self.required_temperature
+
+
+#--------------------------- End of Class Fire ----------------------
 
 
 
@@ -71,7 +67,7 @@ def write_temp_to_file (key):
 def read_remote (debug_on, started_evt ):
     started_evt.set()
     dev = InputDevice ('/dev/input/event0')
-    if debug_on == 1:
+    if debug_on >= 5:
         print (dev)
     for event in dev.read_loop():
     	#
@@ -80,7 +76,7 @@ def read_remote (debug_on, started_evt ):
         # value 0 = key up, 1 = key down, 2 = key hold
 
         if event.type == ecodes.EV_KEY:
-            if debug_on == 1:
+            if debug_on >= 5:
                 print (categorize(event))
                 print ( 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value))
             if event.value == 0:  # key up
@@ -89,9 +85,12 @@ def read_remote (debug_on, started_evt ):
                     time.sleep(1)
                     
 
+#---------------------------------------------------------------------------------
+
+
 my_fire = Fire ()
  
-my_fire.switch_debug_on ()
+my_fire.debug_level_set (1)
 
 my_fire.print_debug_state ()
 
@@ -99,13 +98,13 @@ my_fire.print_debug_state ()
 started_evt = Event()
 
 # Launch the thread and pass the startup event
-print('Launching read_remote')
-t = Thread(target=read_remote, args=(my_fire.debug,started_evt))
+if my_fire.debug_level >=5:
+    print('Launching read_remote')
+t = Thread(target=read_remote, args=(my_fire.debug_level,started_evt))
 t.start()
 
 # Wait for the thread to start
 started_evt.wait()
-print('countdown is running')
 # Infinite loop waiting for input from the flirc
 while True:
     try:
@@ -114,10 +113,10 @@ while True:
         f.close ()
         my_fire.desired_temp_set  (int(temp))
     except IOError:
-        if my_fire.debug:
+        if my_fire.debug_level >=2:
     	    print ("Cant open file")
     		
-    if my_fire.debug:
+    if my_fire.debug_level >= 2:
     	print(str (my_fire.desired_temp_get()))
 	
     time.sleep(2)
