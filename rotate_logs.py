59d21ed6d00a0770b38c771be933dad705aa857a
grimt <graeme.john.thomson@gmail.com>
2015-05-27 10:00:56 +1200

    Create rotate_logs.py
    To be folded in to control_fire.py

---
+++ rotate_logs.py
@@ -0,0 +1,23 @@
+# File to investigate rotating the logs
+
+import logging
+import logging.handlers
+
+LOG_FILENAME = '/tmp/log/logging_rotatingfile_example.out'
+
+# Set up a specific logger with our desired output level
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s  %(message)s')
+
+# Add the log message handler to the logger
+handler = logging.handlers.RotatingFileHandler(
+              LOG_FILENAME, maxBytes=20, backupCount=5)
+
+handler.setFormatter(formatter)
+
+my_logger.addHandler(handler)
+
+# Log some messages
+for i in range(20):
+    my_logger.debug('i = %d' % i)
